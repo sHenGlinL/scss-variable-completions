dcc24ed100297213a15fb5771d8a24c2a6c9449e
liangsl <641579182@qq.com>
2024-12-20 11:02:07 +0800

    fix: 修复bug

--- src/utils.ts
+++ src/utils.ts
@@ -15,36 +15,41 @@ export const getActiveRootPath = (firstFolderPath = CACHE.activeRootPath) => {
 
 // 读取并解析 SCSS 变量文件
 export const parseScssVariables = () => {
-  // 获取当前工作目录
-  const rootPath = getActiveRootPath();
-  //  获取配置
-  const config = workspace.getConfiguration("scss-variable-completions");
-  const relativePaths = config.get<string[]>("variableScssPaths") || [""];
-  // 生成scss文件的绝对路径
-  const filePaths = relativePaths.map((relativePath) =>
-    path.join(rootPath, relativePath)
-  );
-  // 遍历解析scss文件
-  const variableList: { color: string; variable: string }[] = [];
+  try {
+    // 获取当前工作目录
+    const rootPath = getActiveRootPath();
+    //  获取配置
+    const config = workspace.getConfiguration("scss-variable-completions");
+    const relativePaths = config.get<string[]>("variableScssPaths") || [""];
+    // 生成scss文件的绝对路径
+    const filePaths = relativePaths.map((relativePath) =>
+      path.join(rootPath, relativePath)
+    );
+    // 遍历解析scss文件
+    const variableList: { color: string; variable: string }[] = [];
 
-  for (let filePath of filePaths) {
-    const data = fs.readFileSync(filePath, "utf-8");
-    const regex = /\$([\w-]+):\s*(#[0-9a-fA-F]+);/g;
-    let match;
-    while ((match = regex.exec(data)) !== null) {
-      variableList.push({
-        color: match[2],
-        variable: `$${match[1]}`,
-      });
+    for (let filePath of filePaths) {
+      const data = fs.readFileSync(filePath, "utf-8");
+      const regex = /\$([\w-]+):\s*(#[0-9a-fA-F]+);/g;
+      let match;
+      while ((match = regex.exec(data)) !== null) {
+        variableList.push({
+          color: match[2],
+          variable: `$${match[1]}`,
+        });
+      }
     }
-  }
 
-  return variableList;
+    return variableList;
+  } catch (error) {
+    return [];
+  }
 };
 
 // 读取并解析 CSS 变量文件
 export const parseCssVariables = () => {
-  // 获取当前工作目录
+  try {
+    // 获取当前工作目录
   const rootPath = getActiveRootPath();
   //  获取配置
   const config = workspace.getConfiguration("scss-variable-completions");
@@ -66,8 +71,10 @@ export const parseCssVariables = () => {
   for (let filePath of filePaths) {
     const data = fs.readFileSync(filePath, "utf-8");
     const match = themeRegex.exec(data);
-    if (!match) {continue;}
-    
+    if (!match) {
+      continue;
+    }
+
     let variableMatch;
     while ((variableMatch = variableRegex.exec(match[1])) !== null) {
       const variable = variableMatch[1].trim();
@@ -89,4 +96,7 @@ export const parseCssVariables = () => {
   }
 
   return variableList;
+  } catch (error) {
+    return [];
+  }
 };
